6e3f2e7d4ddbd789ad1142c841fd8628c6e1d1cc
Alexey-coder <alexey.user.1988@gmail.com>
2018-07-21 20:01:01 +0300

    initual

---
+++ first/form.pas
@@ -0,0 +1,31 @@
+unit form;
+
+interface
+
+uses
+  Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
+  Dialogs, StdCtrls, Buttons;
+
+type
+  TForm1 = class(TForm)
+    BitBtn1: TBitBtn;
+    procedure BitBtn1Click(Sender: TObject);
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.dfm}
+
+procedure TForm1.BitBtn1Click(Sender: TObject);
+begin
+ShowMessage('gut');
+end;
+
+end.
